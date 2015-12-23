cc164e1496cba550b692a7bbbfaf5568da0b785d
Takafumi Ikeda <ikeike443@github.com>
2015-12-24 02:10:11 +0900

    First commit

---
+++ src/hello-pebble.c
@@ -0,0 +1,43 @@
+#include <pebble.h>
+
+Window *window;
+TextLayer *text_layer;
+
+void init() {
+  // Create the Window
+  window = window_create();
+
+  // Create the TextLayer, for display at (0, 0),
+  // and with a size of 144 x 40
+  text_layer = text_layer_create(GRect(0, 0, 144, 40));
+  // Set the text that the TextLayer will display
+  text_layer_set_text(text_layer, "Hello, Pebble!");
+
+  // Add as child layer to be included in rendering
+  layer_add_child(window_get_root_layer(window),
+                  text_layer_get_layer(text_layer));
+
+  // Push to the stack, animated
+  window_stack_push(window, true);
+}
+
+void deinit() {
+  // Destroy the TextLayer
+  text_layer_destroy(text_layer);
+  // Destroy the Window
+  window_destroy(window);
+}
+
+int main(void) {
+  // Initialize the app
+  init();
+
+  // Wait for app events
+  app_event_loop();
+
+  // Deinitialize the app
+  deinit();
+
+  // App finished without error
+  return 0;
+}
